5f861529a099bb088e34601518c57c00048021da
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-03-14 10:53:41 +0800

    [DELETION] - Removed legacy Flask Server API Logic, and shifted to support ReactJS architecture

--- server/src/show/app.py
+++ server/src/show/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, json
+from flask import Flask, json, request
 from flask_cors import CORS
 from pymongo import MongoClient
 import datetime
@@ -26,55 +26,57 @@ client = MongoClient(MONGO_CONNECTION_STRING)
 db = client["meteo"]
 test_collection = db["test"]
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
 @app.route("/by_station", methods=["GET"])
 def by_station():
-    selected_station = request.args.get("station")
-    if not selected_station:
-        return json.jsonify({"error": "Station parameter is required"}), 400
+    if request.method == "GET":
+        selected_station = request.args.get("station")
+        if not selected_station:
+            return json.jsonify({"error": "Station parameter is required"}), 400
 
-    try:
-        data = list(test_collection.find({"Station": int(selected_station)})
-            .sort("Date", -1)
-            .limit(50)
-        )
-        data.reverse()
-        return json.jsonify([{
-            "Avg": doc["Avg"],
-            "Date": doc["Date"].strftime("%Y-%m-%d"),
-            "FDAvg": doc["FDAvg"],
-            "Station": doc["Station"],
-            "_id": str(doc["_id"]),
-        } for doc in data])
-    except Exception as e:
-        return json.jsonify({"error": str(e)}), 500
+        try:
+            data = list(test_collection.find({"Station": int(selected_station)})
+                .sort("Date", -1)
+                .limit(50)
+            )
+            data.reverse()
+            return json.jsonify([{
+                "Avg": doc["Avg"],
+                "Date": doc["Date"].strftime("%Y-%m-%d"),
+                "FDAvg": doc["FDAvg"],
+                "Station": doc["Station"],
+                "_id": str(doc["_id"]),
+            } for doc in data])
+        except Exception as e:
+            return json.jsonify({"error": str(e)}), 500
+    else:
+        return json.jsonify({"error": "Invalid request method"}), 405
 
 @app.route("/by_date", methods=["GET"])
 def by_date():
-    date_str = request.args.get("date")
-    if not date_str:
-        return json.jsonify({"error": "Date parameter is required"}), 400
+    if request.method == "GET":
+        date_str = request.args.get("date")
+        if not date_str:
+            return json.jsonify({"error": "Date parameter is required"}), 400
 
-    try:
-        chosen_date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
-        next_day = chosen_date + datetime.timedelta(days=1)
-        data = list(test_collection.find({
-            "Date": {"$gte": chosen_date, "$lt": next_day},
-            "Station": {"$in": station_list}
-        }))
-        
-        return json.jsonify([{
-            "Avg": doc["Avg"],
-            "Date": doc["Date"].strftime("%Y-%m-%d"),
-            "FDAvg": doc["FDAvg"],
-            "Station": doc["Station"],
-            "_id": str(doc["_id"]),
-        } for doc in data])
-    except Exception as e:
-        return json.jsonify({"error": str(e)}), 500
+        try:
+            chosen_date = datetime.datetime.strptime(date_str, "%Y-%m-%d")
+            next_day = chosen_date + datetime.timedelta(days=1)
+            data = list(test_collection.find({
+                "Date": {"$gte": chosen_date, "$lt": next_day},
+                "Station": {"$in": station_list}
+            }))
+            
+            return json.jsonify([{
+                "Avg": doc["Avg"],
+                "Date": doc["Date"].strftime("%Y-%m-%d"),
+                "FDAvg": doc["FDAvg"],
+                "Station": doc["Station"],
+                "_id": str(doc["_id"]),
+            } for doc in data])
+        except Exception as e:
+            return json.jsonify({"error": str(e)}), 500
+    else:
+        return json.jsonify({"error": "Invalid request method"}), 405
 
 if __name__ == "__main__":
     app.run(debug=True)
