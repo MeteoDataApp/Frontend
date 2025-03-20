d0979b34ddd1dc1eb63d79c56b9637351513b1a8
lincoln0623 <lincolnlim267@gmail.com>
2025-03-20 10:26:50 +0800

    [Enhancement] - Delayed transition timing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -137,7 +137,7 @@ const Navbar = () => {
                                             initial={{ x: 20, opacity: 0 }}
                                             animate={{ x: 0, opacity: 1 }}
                                             transition={{
-                                                delay: index * 0.1,
+                                                delay: index * 0.2,
                                                 type: "spring",
                                                 stiffness: 100
                                             }}
