ef7aa36bbc3442ee7bb2101aef0afe626967ce38
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-03-28 12:13:05 +0800

    Updated backend url to follow env var

--- networking.js
+++ networking.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:5000", // change this to actual server URL in deployment stage
+    baseURL: import.meta.env.BACKEND_URL,
 })
 
 instance.interceptors.request.use((config) => {
