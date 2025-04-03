7b28fe1bd03009ee3eb2ac7429f6a057cc847127
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-04-03 10:11:48 +0800

    switched to env

--- networking.js
+++ networking.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://meteo-data-backend.vercel.app",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
 })
 
 instance.interceptors.request.use((config) => {
